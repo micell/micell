312c89df0cd1dbd17ab49789fe63a07c900b541f
Alex Chao <alexchao1990@gmail.com>
2019-03-23 13:54:05 +0800

    Rename file name: date/dateFormat -> date/format

--- test/date/format.spec.js
+++ test/date/format.spec.js
@@ -1,13 +1,13 @@
-import dateFormat from '../../src/date/dateFormat';
+import format from '../../src/date/format';
 import padStart from '../../src/_internal/padStart';
 
-describe('date/dateFormat', () => {
+describe('date/format', () => {
   it('should throw error if the first parameter is not a Date object or number', () => {
     expect(() => {
-      dateFormat();
+      format();
     }).toThrow();
     expect(() => {
-      dateFormat('');
+      format('');
     }).toThrow();
   });
 
@@ -15,17 +15,17 @@ describe('date/dateFormat', () => {
     const value = 0;
     const date = new Date(value);
     const hour = padStart(date.getHours(), 2, '0');
-    expect(dateFormat(0, 'YYYY-MM-DDTHH:mm:ss.SSSZ')).toBe(`1970-01-01T${hour}:00:00.000Z`);
+    expect(format(0, 'YYYY-MM-DDTHH:mm:ss.SSSZ')).toBe(`1970-01-01T${hour}:00:00.000Z`);
   });
 
   it('should return the expected format string if the first parameter is a Date object', () => {
     const date = new Date('1970-01-01T00:00:00.000Z');
     const hour = padStart(date.getHours(), 2, '0');
-    expect(dateFormat(date, 'YYYY-MM-DDTHH:mm:ss.SSSZ')).toBe(`1970-01-01T${hour}:00:00.000Z`);
+    expect(format(date, 'YYYY-MM-DDTHH:mm:ss.SSSZ')).toBe(`1970-01-01T${hour}:00:00.000Z`);
   });
 
   it('should return the UTC format string if the third paramter is truth value', () => {
     const date = new Date('1970-01-01T00:00:00.000Z');
-    expect(dateFormat(date, 'YYYY-MM-DDTHH:mm:ss.SSSZ', true)).toBe('1970-01-01T00:00:00.000Z');
+    expect(format(date, 'YYYY-MM-DDTHH:mm:ss.SSSZ', true)).toBe('1970-01-01T00:00:00.000Z');
   });
 });
