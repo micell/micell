84c4a8ff20b14f2354738561ea3568157915e49f
Alex Chao <alexchao1990@gmail.com>
2019-07-02 01:26:21 +0800

    Update the dom index.js

--- src/dom/index.js
+++ src/dom/index.js
@@ -1,19 +1,29 @@
 import clientX from './clientX'
 import clientY from './clientY'
+import createEvent from './createEvent'
+import domReady from './domReady'
+import insertScript from './insertScript'
 import isElement from './isElement'
+import isInViewport from './isInViewport'
 import isWindow from './isWindow'
 import pageX from './pageX'
 import pageY from './pageY'
 import scrollX from './scrollX'
 import scrollY from './scrollY'
+import viewport from './viewport'
 
 export default {
   clientX,
   clientY,
+  createEvent,
+  domReady,
+  insertScript,
   isElement,
+  isInViewport,
   isWindow,
   pageX,
   pageY,
   scrollX,
-  scrollY
+  scrollY,
+  viewport
 }
