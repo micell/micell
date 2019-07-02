eef01e532202939d7b884270be736adc27351f64
Alex Chao <alexchao1990@gmail.com>
2019-07-03 01:41:56 +0800

    Add sinon

--- karma.conf.js
+++ karma.conf.js
@@ -14,6 +14,7 @@ module.exports = function (config) {
     // list of files / patterns to load in the browser
     files: [
       'node_modules/chai/chai.js',
+      'node_modules/sinon/pkg/sinon.js',
       'test/setup.js',
       'test/**/*.spec.js'
     ],
