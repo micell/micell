b6d9f6c9966fe46524123d58750f4d9e653531bd
Alex Chao <alexchao1990@gmail.com>
2019-08-10 20:54:11 +0800

    chore: add semantic-release to release automatically

--- scripts/release.js
+++ scripts/release.js
@@ -1,40 +1,20 @@
-const fs = require('fs')
 const path = require('path')
-const yargsParser = require('yargs-parser')
 const { exec, exit, rm, mkdir, cp } = require('shelljs')
 const { flowRight: compose } = require('lodash')
 const chalk = require('chalk')
-const readline = require('readline-sync')
-const semver = require('semver')
-const pkg = require('../package.json')
 
 const consoleLog = console.log.bind(console)
 const log = compose(consoleLog, chalk.bold)
 const logSuccess = compose(consoleLog, chalk.green.bold)
 const logError = compose(consoleLog, chalk.red.bold)
 
-const argv = yargsParser(process.argv.slice(2))
-const { pkgOnly = false } = argv
 const root = path.resolve(__dirname, '..')
 const outDir = path.resolve(root, 'build')
 
-const writeFile = (filepath, string) =>
-  fs.writeFileSync(filepath, string, 'utf8')
-
 try {
-  if (!pkgOnly && exec('git diff-files --quiet').code !== 0) {
+  if (exec('git diff-files --quiet').code !== 0) {
     logError(`You have unsaved changes in the working tree. Commit or stash changes before releaseing.`)
     exit(1)
-  }
-
-  let nextVersion = readline.question(
-    `Next version of ${pkg.name} (current version is ${pkg.version}):`
-  )
-
-  while (!((semver.valid(nextVersion) && semver.gt(nextVersion, pkg.version)))) {
-    nextVersion = readline.question(
-      `Must provide a valid version that is greater than ${pkg.version}:`
-    )
   }
 
   log('Running tests...')
@@ -69,37 +49,7 @@ try {
     cp('-Rf', src, outDir)
   })
 
-  log('Generating package.json...')
-  const pkgConfig = Object.assign(pkg, { version: nextVersion })
-
-  writeFile(path.resolve(outDir, 'package.json'), JSON.stringify(pkgConfig, null, 2))
-
-  if (pkgOnly) {
-    log('Prepublish done!')
-    exit(0)
-  }
-
-  log('Publishing...')
-  if (exec(`cd ${outDir} && npm publish`).code !== 0) {
-    logError('Publish failed.')
-    exit(1)
-  }
-
-  logSuccess(`${pkg.name}@${nextVersion} was successfully published.`)
-
-  log('Bump version...')
-  exec(`npm --no-git-tag-version version ${nextVersion}`)
-
-  log('Committing changes...')
-  exec('git add package.json')
-  exec(`git commit -m "Release ${nextVersion}"`)
-  exec(`git tag ${nextVersion}`)
-
-  log('Pushing to Github...')
-  exec('git push')
-  exec('git push --tags')
-
-  logSuccess('Done.')
+  logSuccess('Prepare done.')
 } catch (error) {
-  logError('Release failed due to an error', error)
+  logError('Prepare failed due to an error', error)
 }
