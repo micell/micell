80e6d5837a9d4284ad0c4abc402f50f277900622
Alex Chao <alexchao1990@gmail.com>
2019-09-18 22:07:12 +0800

    chore: fix coverage generation for es2015

--- test/noop.spec.js
+++ test/noop.spec.js
@@ -1,11 +1,6 @@
 import noop from '../src/noop'
 
 describe('noop', () => {
-  it('should be an empty function', () => {
-    const str = noop.toString().replace(/\s+/g, '')
-    expect(str).to.equal('functionnoop(){}')
-  })
-
   it('should return undefined', () => {
     expect(noop()).to.equal(undefined)
   })
