cfcebaaf29b77294c617c46feb81a79ec921679b
Alex Chao <alexchao1990@gmail.com>
2020-03-29 01:06:26 +0800

    refactor: rewrite ua modules with typescript

--- src/ua/util/isType.ts
+++ src/ua/util/isType.ts
@@ -9,10 +9,16 @@ const testMap = {
   IOS: /like Mac OS X/,
   Mobile: /Mobi|iPh|480/,
   Tablet: /Tablet|Pad|Nexus 7/,
+  // TODO: named as "Wechat"?
+  // TODO: should supply an alias "Weixin"?
   WeChat: /MicroMessenger/,
   IPhone: /iPhone|iPod/,
   IPad: /iPad/
 }
 const userAgent = isBrowser ? window.navigator.userAgent : ''
 
-export default type => (ua = userAgent) => testMap[type].test(ua)
+export default function (type: string): (ua: string) => boolean {
+  return function (ua: string = userAgent): boolean {
+    return testMap[type].test(ua)
+  }
+}
