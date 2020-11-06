b5585c8c18f5d1df73e5078974d3d537fe855376
Alex Chao <alexchao1990@gmail.com>
2020-11-06 22:55:53 +0800

    feat: add isSymbol method (#51)

---
+++ test/lang/isSymbol.spec.ts
@@ -0,0 +1,16 @@
+import { expect } from 'chai'
+import isSymbol from '../../src/lang/isSymbol'
+
+describe('lang/isSymbol', () => {
+  if (typeof Symbol === 'function') {
+    it('should return true', () => {
+      expect(isSymbol(Symbol('foo'))).to.equal(true)
+    })
+  }
+
+  it('should return false', () => {
+    expect(isSymbol('foo')).to.equal(false)
+    expect(isSymbol(undefined)).to.equal(false)
+    expect(isSymbol(null)).to.equal(false)
+  })
+})
