5962aa2f5abd18faf53c115d68ffc6d95d920b71
Alex Chao <alexchao1990@gmail.com>
2020-11-06 22:42:57 +0800

    feat: add isBigInt method (#50)

---
+++ test/lang/isBigInt.spec.ts
@@ -0,0 +1,16 @@
+import { expect } from 'chai'
+import isBigInt from '../../src/lang/isBigInt'
+
+describe('lang/isBigInt', () => {
+  if (typeof BigInt === 'function') {
+    it('should return true', () => {
+      expect(isBigInt(BigInt(1))).to.equal(true)
+    })
+  }
+
+  it('should return false', () => {
+    expect(isBigInt(1)).to.equal(false)
+    expect(isBigInt(undefined)).to.equal(false)
+    expect(isBigInt(null)).to.equal(false)
+  })
+})
