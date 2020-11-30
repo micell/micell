baf6d7d4664c6e1c7af7da68cfb399439579748c
Alex Chao <alexchao1990@gmail.com>
2020-12-01 00:43:04 +0800

    chore: upgrade devDependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,11 @@ module.exports = {
     mocha: true
   },
   rules: {
+    '@typescript-eslint/ban-ts-comment': 0,
     '@typescript-eslint/ban-ts-ignore': 0,
+    '@typescript-eslint/explicit-module-boundary-types': [1, {
+      allowArgumentsExplicitlyTypedAsAny: true
+    }],
     '@typescript-eslint/no-empty-function': 0,
     '@typescript-eslint/no-explicit-any': 0,
     '@typescript-eslint/no-namespace': 0,
