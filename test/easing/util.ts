34fe222247f8f1bbb831ae82f649b4048497b3e7
Alex Chao <alexchao1990@gmail.com>
2020-12-04 00:04:47 +0800

    test: add more test cases for easing/cubicBezier

--- test/easing/util.ts
+++ test/easing/util.ts
@@ -2,8 +2,16 @@ const identity = (x: number): number => x
 const isApproximate = (a: number, b: number): boolean => Math.abs(a - b) < 0.000001
 type EasingFunc = (x: number) => number
 const compare = (easing1: EasingFunc, easing2: EasingFunc): boolean => {
-  for (let i = 0; i < 100; i++) {
-    if (!isApproximate(easing1(i / 100), easing2(i / 100))) {
+  const nums = [
+    0,
+    // increase coverage
+    0.000001
+  ]
+  for (let i = 1; i <= 100; i++) {
+    nums.push(i / 100)
+  }
+  for (const num of nums) {
+    if (!isApproximate(easing1(num), easing2(num))) {
       return false
     }
   }
