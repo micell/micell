5f6aebc4fc3cb3509847fe5f16b686ae5a554b39
Alex Chao <alexchao1990@gmail.com>
2020-12-05 21:34:03 +0800

    chore: drop browserstack configuration

--- karma.conf.js
+++ karma.conf.js
@@ -1,102 +1,20 @@
 const path = require('path')
-const pkg = require('./package.json')
 
 // Karma configuration
 // Generated on Wed Jun 26 2019 00:32:00 GMT+0800 (China Standard Time)
 
 module.exports = function (config) {
-  const {
-    BS,
-    BROWSERSTACK_USERNAME,
-    BROWSERSTACK_ACCESS_KEY
-  } = process.env
-  const browserStack = {}
   let customLaunchers = {}
-  let reporters = []
 
-  if (BS && BROWSERSTACK_USERNAME && BROWSERSTACK_ACCESS_KEY) {
-    browserStack.username = BROWSERSTACK_USERNAME
-    browserStack.accessKey = BROWSERSTACK_ACCESS_KEY
-    browserStack.project = pkg.name
-
-    reporters = ['dots', 'BrowserStack']
-    customLaunchers = {
-      bs_chrome_win: {
-        base: 'BrowserStack',
-        browser: 'Chrome',
-        browser_version: '77.0',
-        os: 'Windows',
-        os_version: '10'
-      },
-      bs_firefox_win: {
-        base: 'BrowserStack',
-        browser: 'Firefox',
-        browser_version: '69.0',
-        os: 'Windows',
-        os_version: '10'
-      },
-      bs_edge_win: {
-        base: 'BrowserStack',
-        browser: 'Edge',
-        browser_version: '18.0',
-        os: 'Windows',
-        os_version: '10'
-      },
-      bs_safari_mac: {
-        base: 'BrowserStack',
-        browser: 'Safari',
-        browser_version: '12.1',
-        os: 'OS X',
-        os_version: 'Mojave'
-      },
-      // bs_ie11_win: {
-      //   base: 'BrowserStack',
-      //   browser: 'IE',
-      //   browser_version: '11.0',
-      //   os: 'Windows',
-      //   os_version: '8.1'
-      // },
-      // bs_ie10_win: {
-      //   base: 'BrowserStack',
-      //   browser: 'IE',
-      //   browser_version: '10.0',
-      //   os: 'Windows',
-      //   os_version: '8'
-      // },
-      // bs_ie9_win: {
-      //   base: 'BrowserStack',
-      //   browser: 'IE',
-      //   browser_version: '9.0',
-      //   os: 'Windows',
-      //   os_version: '7'
-      // },
-      // bs_ios13: {
-      //   base: 'BrowserStack',
-      //   device: 'iPhone 8',
-      //   real_mobile: 'true',
-      //   os: 'ios',
-      //   os_version: '13'
-      // },
-      bs_android9: {
-        base: 'BrowserStack',
-        device: 'Google Pixel 3',
-        real_mobile: 'true',
-        os: 'android',
-        os_version: '9.0'
-      }
-    }
-  } else {
-    reporters = ['progress', 'coverage']
-    customLaunchers = {
-      ChromeNoSandboxHeadless: {
-        base: 'Chrome',
-        flags: [
-          '--no-sandbox',
-          '--headless',
-          '--disable-gpu',
-          '--remote-debugging-port=9222'
-        ]
-      }
+  customLaunchers = {
+    ChromeNoSandboxHeadless: {
+      base: 'Chrome',
+      flags: [
+        '--no-sandbox',
+        '--headless',
+        '--disable-gpu',
+        '--remote-debugging-port=9222'
+      ]
     }
   }
 
@@ -141,7 +59,7 @@ module.exports = function (config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters,
+    reporters: ['progress', 'coverage'],
 
     coverageReporter: {
       reporters: [
@@ -191,9 +109,6 @@ module.exports = function (config) {
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-    // browserStack global configuration
-    browserStack,
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
