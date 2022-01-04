abb943ac06beb25a316eef42e3006e4256f9ee96
Alex Chao <alexchao1990@gmail.com>
2022-01-04 21:01:29 +0800

    fix: eliminate redos risk

--- src/regex/email.ts
+++ src/regex/email.ts
@@ -1,3 +1,3 @@
 // Ref: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/email#basic_validation
-export default /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+export default /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
 
