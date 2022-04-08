f730d004192f4c1164fc3b6a7deb270914cd094b
Alex Chao <alexchao1990@gmail.com>
2022-04-08 20:31:18 +0800

    feat: add lang.getType method (#128)

---
+++ test/lang/getType.spec.ts
@@ -0,0 +1,18 @@
+import { expect } from 'chai'
+import getType from '../../src/lang/getType'
+
+describe('lang/getType', () => {
+  it('should return the type of value', () => {
+    expect(getType(undefined)).to.equal('Undefined')
+    expect(getType(null)).to.equal('Null')
+    expect(getType(1)).to.equal('Number')
+    expect(getType(true)).to.equal('Boolean')
+    expect(getType('')).to.equal('String')
+    expect(getType(Symbol('foo'))).to.equal('Symbol')
+    expect(getType({})).to.equal('Object')
+    expect(getType([])).to.equal('Array')
+    expect(getType(/\s/)).to.equal('RegExp')
+    expect(getType(new Date())).to.equal('Date')
+    expect(getType(function () {})).to.equal('Function')
+  })
+})
